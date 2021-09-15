c781100e1507fb64c0a5681dd06f34e26e96a033
Maksym Prymierov <maksym.v.prymierov@globallogic.com>
2021-09-15 16:12:05 +0300

    Add entering two numbers from the keyboard
    Signed-off-by: Maksym Prymierov <maksym.v.prymierov@globallogic.com>

--- main.cpp
+++ main.cpp
@@ -2,8 +2,14 @@
 
 int main()
 {
-        int a = 5;
-        int b = 5;
+        int a;
+        int b;
+
+        std::cout << "a: ";
+        std::cin >> a;
+
+        std::cout << "b: ";
+        std::cin >> b;
 
         std::cout << a << " + " << b << " = " << a + b << std::endl;
 
